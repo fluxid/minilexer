2100503af847c2c04feb655a474e0bea6294855f
Tomasz Fluxid Kowalczyk <accreg-git@fluxid.pl>
2011-02-12 11:58:25 +0100

    Typo in PYTHONPATH variable name...

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,5 @@
 WORKING_DIR="$(cd "${0%/*}" 2>/dev/null; dirname "$PWD"/"${0##*/}")"
 cd ${WORKING_DIR}
 
-export PYTHON_PATH="${PWD}/src:${PYTHON_PATH}"
+export PYTHONPATH="${PWD}/src:${PYTHONPATH}"
 nosetests --with-coverage3 --cover3-package=fxd.minilexer --doctest-tests --with-doctest tests
